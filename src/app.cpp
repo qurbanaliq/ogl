cfc4b505984cf21ca7babaa95022f9de5f3c4f6d
Qurban Ali <qurbanaliqureshi@gmail.com>
2020-06-24 19:01:08 +0100

    shaders with hard coded string sources

--- src/app.cpp
+++ src/app.cpp
@@ -55,6 +55,41 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
+	unsigned int shaderProgram = glCreateProgram();
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	std::string vs = "#version 330\n"
+			"layout(location=0) in vec3 position;\n"
+			"void main(void)\n"
+			"{\n"
+			"gl_Position = vec4(0.5 * position, 1.0);\n"
+			"}\n";
+
+	const char* src;
+	src = vs.c_str();
+	glShaderSource(vertexShader, 1, &src, nullptr);
+	glCompileShader(vertexShader);
+
+	std::string fs = "#version 330\n"
+			"out vec4 color;\n"
+			"void main()\n"
+			"{\n"
+			"color = vec4(0.3, 0.2, 0.5, 1.0);\n"
+			"}\n";
+
+	src = fs.c_str();
+	glShaderSource(fragmentShader, 1, &src, nullptr);
+	glCompileShader(fragmentShader);
+
+	//TODO: validate shader here
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glValidateProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	while(!glfwWindowShouldClose(window))
