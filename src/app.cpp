d02f885a10ed7d845f2e177c91c62f90dd7eda61
Qurban Ali <qurbanaliqureshi@gmail.com>
2020-07-09 22:43:34 +0100

    camera movement

--- src/app.cpp
+++ src/app.cpp
@@ -226,23 +226,29 @@ int main(void)
 	float viewX = 0.0f, viewY = 0.0f, viewZ = 0.0f;
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	float radius = 10.0f;
+
+	glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
+	glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
+	glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
+
+	float cameraSpeed = 0.0f;
+
 	while(!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
 		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-			radius += 0.1;
+			cameraPos += cameraFront;
 		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-			radius -= 0.1;
+			cameraPos -= cameraFront;
 		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-			viewX -= 0.1;
+			cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp));
 		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-			viewX += 0.1;
+			cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp));
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			std::cout << (cameraPos + cameraFront).x << " " << (cameraPos + cameraFront).y << " " << (cameraPos + cameraFront).z << std::endl; }
 
-		float camX = sin(glfwGetTime()) * radius;
-		float camZ = cos(glfwGetTime()) * radius;
-		glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), 640.0f/480.0f, 0.1f, 100.0f);
 
 		glEnable(GL_DEPTH_TEST);
